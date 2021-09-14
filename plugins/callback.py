d83a15bb8699ed9c51acb1a51c3d90859963809f
Randy wahidin <63757267+Randi356@users.noreply.github.com>
2021-09-15 01:02:22 +0700

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -224,8 +224,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/tutorial_updates'),
+                InlineKeyboardButton('🧩 Source', url='https://github.com/Randi356/Music-Player'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
